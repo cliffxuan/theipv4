f0095951df826f17f76633341d956ad05c76bd27
cliff xuan <cliff.xuan@gmail.com>
2011-06-01 10:37:17 +0100

    use hothandler

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from google.appengine.ext.webapp import util
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        ip = self.request.remote_addr
+        ip = 'my IP address: ' + self.request.remote_addr
         self.response.out.write(ip)
 
 
